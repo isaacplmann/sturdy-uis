7defa63b64e499f0300c3febdef3a68574a89f53
Isaac Mann <isaacplmann+git@gmail.com>
2019-11-21 19:52:55 -0500

    scaffold the force machine

---
+++ src/machines/force.ts
@@ -0,0 +1,36 @@
+import { Machine } from 'xstate';
+
+// The hierarchical (recursive) schema for the states
+export interface ForceSchema {
+  states: {
+    light: {};
+    dark: {};
+  };
+}
+
+// The events that the machine handles
+export type ForceEvents = { type: 'CORRUPT' } | { type: 'REDEEM' };
+
+// The context (extended state) of the machine
+export interface ForceContext {}
+
+export const forceMachine = Machine<ForceContext, ForceSchema, ForceEvents>(
+  {
+    id: 'force',
+    initial: 'light',
+    context: {},
+    states: {
+      light: {
+        on: {
+          CORRUPT: 'dark'
+        }
+      },
+      dark: {
+        on: {
+          REDEEM: 'light'
+        }
+      }
+    }
+  },
+  {}
+);
